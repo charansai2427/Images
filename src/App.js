8ac79a417f765dfe33103757a2d1190c0e69ac3b
unknown <charan7728264@gmail.com>
2023-07-29 16:22:49 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,15 @@
-import logo from './logo.svg';
 import './App.css';
+import Client from './components/imagegallery';
+import { useState } from 'react';
+import { UseSelector } from 'react-redux/es/hooks/useSelector';
 
 function App() {
+  const [ state,setState] = useState("thunderstorm")
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+<Client query ={state}/>
+  
     </div>
   );
 }
---
+++ src/components/imagegallery.js
@@ -0,0 +1,35 @@
+import {createClient} from "pexels";
+import { useState,useEffect } from "react";
+import "../styles/imagegallery.css"
+import { createSelector,createAsyncThunk } from "@reduxjs/toolkit";
+
+export default function Client ( { query}){
+const client = createClient(' DnS6kIU35UjI6H608t9wBb6HN8ro2GFvK9msrGG0RoP97FdYHE07x4wC');
+const [data,setData]= useState({photos:[]})
+const [check,setcheck ] = useState("")
+const [page,setPage] = useState(1);
+
+// const query = "Beach";
+
+useEffect(()=>{
+    client.photos.search({ query,per_page:200,page}).then(res =>{
+        console.log(res);
+        setData({photos:[...res.photos]});
+    })
+},[page,query])
+
+const [show,setShow] = useState();
+return(
+   <div>
+        <div>
+            {
+                data && data.photos.map((e)=>{
+                    return(
+                        <img onClick={() => {setcheck(e.src);setShow(true)}} src={e.src.original} />
+                    )
+                })
+            }
+        </div>
+        </div>
+)
+}
